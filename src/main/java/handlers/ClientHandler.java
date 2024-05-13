79670355c997bdbfb35a5cd8171868bf51a5ee7f
ishansharma <ishan.sharma@fintheon.com>
2024-05-14 00:22:37 +0530

    Update ClientHandler To Runnable

--- src/main/java/handlers/ClientHandler.java
+++ src/main/java/handlers/ClientHandler.java
@@ -3,22 +3,33 @@ package handlers;
 import java.io.*;
 import java.net.Socket;
 
-public class ClientHandler {
-    public void handle(Socket clientSocket) throws IOException {
-        BufferedReader reader = new BufferedReader(
-                new InputStreamReader(clientSocket.getInputStream()));
-        BufferedWriter writer = new BufferedWriter(
-                new OutputStreamWriter(clientSocket.getOutputStream()));
+public class ClientHandler implements Runnable {
 
-        while (clientSocket.isConnected()) {
-            String line = reader.readLine();
-            if (line == null) {
-                break;
+    Socket clientSocket;
+
+    public ClientHandler(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+            BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(clientSocket.getOutputStream()));
+            while (clientSocket.isConnected()) {
+                String line = reader.readLine();
+                if (line == null) {
+                    break;
+                }
+                if (line.equalsIgnoreCase("PING")) {
+                    writer.write("+PONG\r\n");
+                    writer.flush();
+                }
             }
-            if (line.equalsIgnoreCase("PING")) {
-                writer.write("+PONG\r\n");
-                writer.flush();
-            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
