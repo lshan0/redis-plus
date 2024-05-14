9942eaebadce2d7d000496979d4fad286f75e00c
ishansharma <ishan.sharma@fintheon.com>
2024-05-14 23:44:46 +0530

    Add ECHO and PING functionality

--- src/main/java/handlers/ClientHandler.java
+++ src/main/java/handlers/ClientHandler.java
@@ -5,6 +5,9 @@ import java.net.Socket;
 
 public class ClientHandler implements Runnable {
 
+    private static String PING = "PING";
+
+    private static String ECHO = "ECHO";
     Socket clientSocket;
 
     public ClientHandler(Socket clientSocket) {
@@ -23,13 +26,51 @@ public class ClientHandler implements Runnable {
                 if (line == null) {
                     break;
                 }
-                if (line.equalsIgnoreCase("PING")) {
-                    writer.write("+PONG\r\n");
-                    writer.flush();
+                if (line.charAt(0) == '*') {
+                    int argNumber = Integer.parseInt(line.substring(1));
+                    if (argNumber == 0) {
+                        break;
+                    }
+                    String instruction = readArg(reader);
+                    int readArg = 1;
+                    if (PING.equalsIgnoreCase(instruction)) {
+                        readArg += handlePING(writer);
+                    } else if (ECHO.equalsIgnoreCase(instruction)) {
+                        readArg += handleECHO(reader, writer);
+                    }
+                    if (readArg < argNumber) {
+                        throw new IllegalStateException("There are some args that should be read");
+                    }
                 }
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private int handlePING(BufferedWriter writer) throws IOException {
+        writer.write("+PONG\r\n");
+        writer.flush();
+        return 0;
+
+    }
+
+    private String readArg(BufferedReader reader) throws IOException {
+        String numberBytesLines = reader.readLine();
+        if (numberBytesLines.charAt(0) != '$') {
+            throw new IllegalStateException();
+        }
+        return reader.readLine();
+    }
+
+    private int handleECHO(BufferedReader reader, BufferedWriter writer) throws IOException{
+        String message = readArg(reader);
+        writer.write(toRedisProtocolMessage(message));
+        writer.flush();
+        return 1;
+    }
+
+    private String toRedisProtocolMessage(String message) {
+        return "$" + message.length() + "\r\n" + message + "\r\n";
     }
 }
